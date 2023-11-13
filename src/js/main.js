a35360371be0380ec7ca3162dbeadf25a939eae8
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2023-11-13 12:30:10 +0200

    Fix : slider

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,16 @@ import burger from '../components/blocks/header/top/burger/burger.js';
 import order from '../components/blocks/header/order/_order.js';
 import navProduct from '../components/blocks/header/nav-product/_nav-product.js';
 import seeds from '../components/blocks/main/seeds/_seeds.js';
-import carousel from './modules/carousel.js';
+// import carousel from './modules/carousel.js';
+import novelties from '../components/blocks/main/novelties/_novelties.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    carousel();
-  // burger();
-  // order();
-  // navProduct();
-  // seeds();
-  // novelties();
+    novelties();
+    // carousel();
+  burger();
+  order();
+  navProduct();
+  seeds();
 });
 
 // /**
